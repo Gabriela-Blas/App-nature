8737a8cb192b3a9181c317c61f86d704adf34d8c
Gabriela <gabriela.blas001@gmail.com>
2022-08-04 15:10:56 -0500

    agreagando rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./views/Home.js"
+import Product from "./views/Product.js"
+import Scan from "./views/Scan.js"
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/Product" element={<Product />} />
+                <Route path="/Scan" element={<Scan />} />
             </Routes>
         </BrowserRouter>
     );
